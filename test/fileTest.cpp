d26217d5ce4a968b986beaebead55085a1a269ca
Sho Ito <35167423+Sean0628@users.noreply.github.com>
2025-04-18 12:46:24 -0400

    Add catch2 for testing (#3)
    * chore: add .gitignore
    
    * chore: remove version
    
    now it is not supported
    
    * test: add catch2 support
    
    use catch2 for testing
    
    * chore: add GH actions config
    
    * fix: add missing dependencies
    
    * test: fix not to include cpp files

--- test/fileTest.cpp
+++ test/fileTest.cpp
@@ -1,28 +1,45 @@
-#include "app/SampleDB.cpp"
-#include "file/BlockId.cpp"
-#include "file/FileMgr.cpp"
-#include "file/Page.cpp"
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
-int main() {
-  app::SampleDB db("filetest", 400, 8);
+#include <string>
+#include "app/SampleDB.h"
+#include "file/BlockId.h"
+#include "file/FileMgr.h"
+#include "file/Page.h"
+
+std::pair<std::string, int> writeThenReadBack(
+  const std::string& dbName,
+  const std::string& fileName,
+  int blockNum,
+  int pos,
+  const std::string& valueStr,
+  int valueInt
+) {
+  app::SampleDB db(dbName, 400, 8);
   file::FileMgr& fm = db.fileMgr();
-  file::BlockId blk("testfile", 2);
-  int pos1 = 88;
+  file::BlockId blk(fileName, blockNum);
 
   file::Page p1(fm.blockSize());
-  string s = "abcdefghijklm";
-  p1.setString(pos1, s);
-  int size = file::Page::maxLength(s.size());
+  p1.setString(pos, valueStr);
 
-  int pos2 = pos1 + size;
-  p1.setInt(pos2, 345);
+  int pos2 = pos + file::Page::maxLength(valueStr.size());
+  p1.setInt(pos2, valueInt);
+
   fm.write(blk, p1);
 
   file::Page p2(fm.blockSize());
   fm.read(blk, p2);
 
-  cout << "offset " << pos2 << " contains " << p2.getInt(pos2) << endl;
-  cout << "offset " << pos1 << " contains " << p2.getString(pos1) << endl;
+  return {p2.getString(pos), p2.getInt(pos2)};
+}
 
-  return 0;
+TEST_CASE("String and int values can be written and read back") {
+  std::string str = "abcdefghijklm";
+  int number = 345;
+  int pos = 88;
+
+  auto [readStr, readInt] = writeThenReadBack("filetest", "testfile", 2, pos, str, number);
+
+  REQUIRE(readStr == str);
+  REQUIRE(readInt == number);
 }
