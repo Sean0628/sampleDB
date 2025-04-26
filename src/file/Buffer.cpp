51ebdf57afa8333e3b491c022cae5003825bd7a8
Sho Ito <i.sho.0628@gmail.com>
2025-04-26 11:39:59 -0400

    add BufferMgr, Buffer class

---
+++ src/file/Buffer.cpp
@@ -0,0 +1,42 @@
+#include "file/Buffer.h"
+
+file::Buffer::Buffer(file::FileMgr& fileMgr, file::LogMgr& logMgr)
+  : _fileMgr(fileMgr), _logMgr(logMgr), _page(std::make_unique<file::Page>(fileMgr.blockSize())), 
+    _blk(file::BlockId("", -1)) {
+}
+
+void file::Buffer::setModified(int txnum, int lsn) {
+  _txnum = txnum;
+  if (lsn >= 0) _lsn = lsn;
+}
+
+bool file::Buffer::isPinned() {
+  return _pins > 0;
+}
+
+int file::Buffer::modifyingTx() {
+  return _txnum;
+}
+
+void file::Buffer::assignToBlock(file::BlockId& blk) {
+  flush();
+  _blk = blk;
+  _fileMgr.read(_blk, *_page);
+  _pins = 0;
+}
+
+void file::Buffer::flush() {
+  if (_txnum >= 0) {
+    _logMgr.flush(_lsn);
+    _fileMgr.write(_blk, *_page);
+    _txnum = -1;
+  }
+}
+
+void file::Buffer::pin() {
+  _pins++;
+}
+
+void file::Buffer::unpin() {
+  _pins--;
+}
---
+++ src/file/BufferMgr.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <mutex>
+#include <condition_variable>
+#include "file/FileMgr.h"
+#include "file/BlockId.h"
+#include "file/LogMgr.h"
+#include "file/Buffer.h"
+
+namespace file {
+  class BufferMgr {
+    public:
+      BufferMgr(file::FileMgr& fileMgr, file::LogMgr& logMgr, int buffSize);
+      int available();
+      void flushAll(int txnum);
+      void unpin(Buffer& buff);
+      Buffer* pin(BlockId& blk);
+    private:
+      const int MAX_TIME = 10000; // 10 seconds
+      std::vector<std::unique_ptr<Buffer>> _bufferpool;
+      int _numAvailable;
+      std::mutex _mutex;
+      std::condition_variable _cv;
+      bool waitingTooLong(std::chrono::steady_clock::time_point start);
+      Buffer* tryToPin(BlockId& blk);
+      Buffer* findExistingBuffer(BlockId& blk);
+      Buffer* chooseUnpinnedBuffer();
+  };
+}
